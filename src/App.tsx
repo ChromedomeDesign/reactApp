e57b38ce8485651023e9e93b70eb7c72d9430795
mlawrence <mike@neonglyph.com>
2023-08-24 22:40:30 -0400

    adding MDB to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+// @ts-expect-error moduleResolution:nodenext issue 54523
+import { MDBContainer, MDBBtn } from 'mdb-react-ui-kit'
 
 type AppProps = {
    message: string
@@ -6,9 +8,30 @@ type AppProps = {
 
 const App = ({message}: AppProps): JSX.Element => {
    return (
-      <div>
-         <h1>{message}</h1>
+      <MDBContainer fluid>
+      <div
+        className='d-flex justify-content-center align-items-center'
+        style={{ height: '100vh' }}
+      >
+        <div className='text-center'>
+          <img
+            className='mb-4'
+            src='https://mdbootstrap.com/img/logo/mdb-transparent-250px.png'
+            style={{ width: 250, height: 90 }}
+          />
+          <h5 className='mb-3'>{message}</h5>
+          <p className='mb-3'>MDB Team</p>
+          <MDBBtn
+            tag='a'
+            href='https://mdbootstrap.com/docs/standard/getting-started/'
+            target='_blank'
+            role='button'
+          >
+            Click this Button
+          </MDBBtn>
+        </div>
       </div>
+    </MDBContainer>
    )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client'
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import App from './App.tsx';
 
 const container = document.getElementById('app')
 const root = createRoot(container)
 
-root.render(<App message='Hello World' />)
+root.render(<App message='Pizza Steve' />)
