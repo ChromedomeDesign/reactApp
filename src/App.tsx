6fe7be9d20a549e2f541041a2c725b899dc3d582
mlawrence <mike@neonglyph.com>
2023-08-28 10:38:45 -0400

    added in form elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // @ts-expect-error
-import { MDBContainer, MDBBtn } from 'mdb-react-ui-kit';
+import { MDBContainer, MDBBtn, MDBTextArea, MDBInput } from 'mdb-react-ui-kit';
 
 type AppProps = {
     message: string;
@@ -38,6 +38,69 @@ const App = ({ message }: AppProps): JSX.Element => {
                 </div>
                 <div className="overlay"></div>
             </header>
+            <MDBContainer fluid className="content-section" id="about">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-10 mx-auto">
+          <h2 className="text-center">Contact Us</h2>
+          <form id="contact-form" method="post" action="contact.php" role="form">
+
+                    <div className="messages"></div>
+
+                    <div className="controls">
+                        <div className="row">
+                            <div className="col-lg-6">
+                                <div className="form-group">
+                                    <label htmlFor="form_name">Name *</label>
+                                    <MDBInput label='Please enter your name *' className="form-control" id='form_name' type='text' />
+                                    <div className="help-block with-errors"></div>
+                                </div>
+                            </div>
+                            <div className="col-lg-6">
+                                <div className="form-group">
+                                    <label htmlFor="form_lastname">Company</label>
+                                    <MDBInput label='Please enter your company name' className="form-control" id='form_company' type='text' />
+                                    <div className="help-block with-errors"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-lg-6">
+                                <div className="form-group">
+                                    <label htmlFor="form_email">Email *</label>
+                                    <MDBInput label='Please enter your email *' className="form-control" id='form_email' type='email' />
+                                    <div className="help-block with-errors"></div>
+                                </div>
+                            </div>
+                            <div className="col-lg-6">
+                                <div className="form-group">
+                                    <label htmlFor="form_phone">Phone</label>
+                                    <MDBInput label='Please enter your phone' className="form-control" id='form_phone' type='tel' />
+                                    <div className="help-block with-errors"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="form_message">Message *</label>
+                            <MDBTextArea label='Message' id='textAreaExample' rows={4} className="form-control" />
+                            <div className="help-block with-errors"></div>
+                        </div>
+                            
+                    <MDBBtn tag="a" href="https://chromedomedesign.com/" target="_blank" role="button">
+                        Send message
+                    </MDBBtn>
+
+                        <p className="text-muted">
+                            <strong>*</strong> These fields are required.
+                        </p>
+
+                    </div>
+
+                </form>
+        </div>
+      </div>
+    </div>
+            </MDBContainer>
         </MDBContainer>
     );
 };
