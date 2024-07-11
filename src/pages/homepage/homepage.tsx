5b92b33b0b94e58c8a1967ec04a9431fc5536b8b
Dustin Murphy <dustin@generasystems.com>
2024-07-11 14:04:28 -0700

    Fixing Dependency issues

--- src/pages/homepage/homepage.tsx
+++ src/pages/homepage/homepage.tsx
@@ -3,10 +3,10 @@ import React from "react"
 import { MDBContainer, MDBBtn } from "mdb-react-ui-kit"
 import { ContactForm } from "@feature/contact-form/contact-form"
 // static assets
-import head1 from "img/head-1.png"
-import head2 from "img/head-2.png"
-import head3 from "img/head-3.png"
-import head4 from "img/head-4.png"
+import head1 from "./img/head-1.png"
+import head2 from "./img/head-2.png"
+import head3 from "./img/head-3.png"
+import head4 from "./img/head-4.png"
 
 export const Homepage = (props: any): JSX.Element => {
     return (
@@ -16,16 +16,16 @@ export const Homepage = (props: any): JSX.Element => {
                     <h1 className="mb-5">chromedome design</h1>
                     <div className="row mb-5">
                         <div className="col-lg-3 col-md-6 mb-5 mb-lg-0">
-                            <img src={head1} />
+                            <img title="chromedome head 1" src={head1} />
                         </div>
                         <div className="col-lg-3 col-md-6 mb-5 mb-lg-0">
-                            <img src={head2} />
+                            <img title="chromedome head 2" src={head2} />
                         </div>
                         <div className="col-lg-3 col-md-6 mb-5 mb-md-0">
-                            <img src={head3} />
+                            <img title="chromedome head 3" src={head3} />
                         </div>
                         <div className="col-lg-3 col-md-6">
-                            <img src={head4} />
+                            <img title="chromedome head 4" src={head4} />
                         </div>
                     </div>
                     <h3 className="mb-5">
